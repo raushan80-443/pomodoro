63b5e4d08998a2ad8825a1592c29734cda54a064
Raushan Kumar <raushan102188@gmail.com>
2023-04-21 23:25:08 +0530

    short code

--- main1.py
+++ main1.py
@@ -1,14 +1,9 @@
 import time
 import tkinter as tk
-import pygame
-#play song game over
-pygame.mixer.init()
-pygame.mixer.music.load("gameover.mp3")
-pygame.mixer.music.play()   
 
 # set the time interval for work and break periods
-work_time = 20 * 60  # 20 minutes
-break_time = 2 * 60  # 2 minutes
+work_time = 25 * 60  # 25 minutes
+break_time = 2.5 * 60  # 2.5 minutes
 
 # function to lock the screen
 def lock_screen():
@@ -25,33 +20,13 @@ def lock_screen():
     # update the window and wait for the break period to end
     root.update()
     time.sleep(break_time -10)
-    unlock_screen()
-
-
+    label = tk.Label(root, text="Get back to work!", font=("Helvetica", 36) ,fg="green",bg="black")
+    label.pack(expand=True)
+    
     # destroy the window and unlock the screen
     root.destroy()
 
 # function to unlock the screen
-def unlock_screen():
-    # create a new window and make it full screen
-    root = tk.Tk()
-    root.attributes('-topmost', True)
-    root.attributes('-fullscreen', True)
-    root.configure(bg='black')
-
-
-    # add a label to display the unlock message
-    label = tk.Label(root, text="Get back to work!", font=("Helvetica", 36) ,fg="green",bg="black")
-    label.pack(expand=True)
-
-    # update the window and wait for the work period to end
-    root.update()
-    time.sleep(3)
-
-    # destroy the window and lock the screen
-    root.destroy()
-    # lock_screen()
-
 # start the Pomodoro timer
 a = 0
 while a < 6:
@@ -63,7 +38,6 @@ while a < 6:
     print("Starting break period...")
     lock_screen()
     #
-    unlock_screen()
     a += 1
 # lock_screen()
 
